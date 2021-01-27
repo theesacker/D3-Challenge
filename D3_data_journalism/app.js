cab205ac10f4236470959df5009c7eea292429a0
Tyson Heesacker <tysonheesacker86@gmail.com>
2021-01-26 20:35:40 -0800

    finalizing toolTip

--- D3_data_journalism/app.js
+++ D3_data_journalism/app.js
@@ -30,37 +30,78 @@ var chartGroup = svg.append("g")
 d3.csv("data.csv").then(function(npData){
     console.log(npData)
     npData.forEach(function(data){
-      data.pov = +data.poverty
-      data.health = +data.healthcare
+      data.poverty = +data.poverty
+      data.healthcare = +data.healthcare
       // console.log(data.health)
-      console.log(data.pov)
+      // console.log(data.poverty)
 
 
    }); 
-var xScalePoverty = d3.scaleBand()
-.domain([0,d3.max(npData,function(d){
-  return +d.pov;
+var xScalePoverty = d3.scaleLinear()
+.domain([8,d3.max(npData,function(d){
+  return +d.poverty;
 })])
 .range([0, chartWidth]);
 
 var yScaleHealthCare = d3.scaleLinear()
 .domain([0, d3.max(npData, function(d){
-  return +d.health;
+  return +d.healthcare;
 })])
 .range([chartHeight,0]);
-
+// ---------------------------------------------------------------
 // console.log(yScaleHealthCare);
-// console.log(xScalePoverty);
-// Scales and axes
+console.log(xScalePoverty);
+// // Scales and axes
 var bottomAxis = d3.axisBottom(xScalePoverty)
-// .tickFormat()
+// // .tickFormat()
 var leftAxis = d3.axisLeft(yScaleHealthCare)
 // Add x-Axis
 chartGroup.append("g")
-    .attr("transform", `translate(0, ${height})`)
+    // .classed("axis", true)
+    .attr("transform", `translate(0, ${chartHeight})`)
     .call(bottomAxis);
-// Add y_axis
+// // Add y_axis
 chartGroup.append("g")
-call(leftAxis);
+.classed("axis", true)
+.call(leftAxis);
+
+// Line generators
+var drawLine = d3.line()
+.x((d,i) => xScalePoverty(i))
+.y(d => yScaleHealthCare(d))
+// console.log(drawLine(npData));
+// // append path for lines
+chartGroup.append("path")
+.attr("d", (drawLine(npData)));
+// .classed("line", true));
+
+
+// Create circles to plot
+circlesGroup = chartGroup.selectAll("circle")
+.data(npData)
+.enter()
+.append("circle")
+.attr("cx", (d,i) => xScalePoverty(d.poverty))
+.attr("cy", d => yScaleHealthCare(d.healthcare))
+.attr("fill", "blue")
+.attr("r", "15");
+
+var toolTip = d3.select("body")
+.append("div")
+.attr("class", "tooltip");
+// circlesGroup.call(toolTip)
+
+
+circlesGroup.on("mouseover", function(d){
+  d3.select(this)
+  toolTip.style("display", "block");
+  toolTip.html(`${d.abbr} <hr> Healthcare (%) ${d.healthcare} <hr> Poverty ${d.poverty} `)
+  // .style("left", d3.event.pageX + "px")
+  // .style("top", d3.event.pageY + "px");
+})
+.on("mouseout", function(d,i){
+  d3.select(this)
+  toolTip.style("display", "none");
+});
 
 });
